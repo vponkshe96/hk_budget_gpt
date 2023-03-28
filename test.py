4adc7359e83e938345b9ed560a29e544ef3de2ba
vponkshe96 <vpponkshe@gmail.com>
2023-03-28 11:35:43 +0800

    minor changes

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-qs1 = "What benefits do HKers get this year?"
-qs2 = "Explain to me why do I even need to care about this document?"
-q3 = "Summarize this document"
-q4 = "What's the gov doing to support startups?"
-q5 = "You are a Goldman Sachs analyst, using the report write a short commentary on the hong kong economy"
+q1 = "What benefits do HKers get this year?"
+q2 = "Explain to me why do I even need to care about this document?"
+q3 = "What was mentiond about InvestHK "
+q4 = "What is the gov doing to boost the tourism sector?"
+q5 = "You are a Goldman Sachs analyst, write a short commentary on the HK economy."
 
 
 test_answer = " You should care because the government is proposing to increase the amount of monthly allowance for carers from low-income families and set up a dedicated carer support hotline with professional social workers to provide emergency support, emotional counselling, and outreaching services. They are also proposing to increase the tax deduction for the Mandatory Provident Fund (MPF) voluntary contributions made by employers for their employees aged 65 or above, from the current 100 per cent to 200 per cent in respect of such expenditure. This will encourage employers to continue to hire mature employees, while helping the silver-haired increase their retirement savings."
